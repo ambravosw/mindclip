99a6af71a35824beb01b0a89bb99f571ba25f60e
amb <majete@gmail.com>
2020-05-15 14:05:41 +0200

    deploy

---
+++ precache-manifest.4c0ba8eeb7a7da323737bff8d0ea97d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "377475e845981526e6a1",
+    "url": "/mindclip/css/chunk-vendors.a489faba.css"
+  },
+  {
+    "revision": "cef51ad0fe17cd9b5ae0c99ca7ae44e4",
+    "url": "/mindclip/index.html"
+  },
+  {
+    "revision": "ef686522739a24baee5a",
+    "url": "/mindclip/js/app.4941f816.js"
+  },
+  {
+    "revision": "377475e845981526e6a1",
+    "url": "/mindclip/js/chunk-vendors.ced2118b.js"
+  },
+  {
+    "revision": "5871cb728b8e48580a52451b95a5b2bf",
+    "url": "/mindclip/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/mindclip/robots.txt"
+  }
+]);
---
+++ service-worker-mindclip.js
@@ -0,0 +1,47 @@
+importScripts("/mindclip/precache-manifest.4c0ba8eeb7a7da323737bff8d0ea97d1.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+workbox.core.setCacheNameDetails({prefix: "mindclip"});
+
+self.addEventListener('message', (event) => {
+    if (event.data && event.data.type === 'SKIP_WAITING') {
+        self.skipWaiting();
+    }
+});
+
+// Background sync
+const bgSyncPlugin = new workbox.backgroundSync.Plugin('mindclip', {
+    maxRetentionTime: 0.1 * 60 // Retry for max of 24 Hours
+});
+
+workbox.routing.registerRoute(
+    // 'http://192.168.1.193:8000/api/test', // con la ruta funciona, hacer regExp
+    new RegExp('http://192.168.1.193:8000/api/.*'),
+    workbox.strategies.networkOnly({
+        plugins: [bgSyncPlugin]
+    }),
+    'POST'
+);
+
+// self.addEventListener('sync', event => {
+//     console.log('syncing',event)
+// });
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
